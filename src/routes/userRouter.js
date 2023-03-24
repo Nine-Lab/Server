719c336f11aa07129259b9117b020b0345d2dd94
Junhyun Park <netscyber2004@gmail.com>
2023-03-25 01:19:57 +0900

    mypage, update

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { userController } from "../controllers/userController.js";
+import { myPage } from "../services/myPage.js";
 
 const userRouter = express.Router();
 
@@ -8,5 +9,8 @@ userRouter.post("/users/register", userController.register);
 
 // 로그인
 userRouter.post("/users/login", userController.login);
+
+// 마이페이지
+userRouter.post("/users/mypage", myPage);
 
 export default userRouter;
