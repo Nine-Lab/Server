66942648d8a5688eb1079bff957170b20c99bce1
Hee <heehanhand@gmail.com>
2023-04-27 02:16:03 +0900

    feat: 페이지네이션 기능 추가

---
+++ src/db/models/reviewModel.js
@@ -0,0 +1,37 @@
+import { model } from "mongoose";
+
+import ReviewSchema from "../schemas/reviewSchema";
+
+const Review = model("reviews", ReviewSchema);
+
+export class ReviewModel {
+  //  리뷰 추가
+  async create(reviewInfo) {
+    const createdNewReview = await Review.create(reviewInfo);
+    return createdNewReview;
+  }
+
+  //  리뷰 삭제
+  async deleteById(reviewId) {
+    const deleteResult = await Review.deleteOne({ _id: reviewId });
+    return deleteResult;
+  }
+
+  //  리뷰 조회
+  async getAllReviews() {
+    const reviews = await Review.find({});
+    return reviews;
+  }
+
+  //  리뷰 수정
+  async updateById(reviewParams) {
+    const id = reviewParams.reviewId;
+    delete reviewParams.reviewId;
+    const updateResult = await Review.findOneAndUpdate({ _id: id }, { $set: reviewParams }); 
+    return updateResult;
+  }  
+}
+
+const reviewModel = new ReviewModel();
+
+export { reviewModel };
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+import reviewRouter from "./reviewRouter";
+import userRouter from "./userRouter";
+
+
+export { userRouter, reviewRouter };
